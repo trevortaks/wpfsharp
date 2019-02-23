b35e79ab4dcfb7ff6a4cad82d4fbfa941b0a4993
Trevor Takawira <trevortaks@gmail.com>
2019-02-23 22:16:34 +0200

    Initial Commit

---
+++ instasharp/User.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstaSharper;
+using InstaSharper.Classes;
+using InstaSharper.Classes.Models;
+using InstaSharper.API.Builder;
+using InstaSharper.Logger;
+using InstaSharper.API;
+using System.IO;
+
+namespace instasharp
+{
+    public class User
+    {
+        private static IInstaApi _instaApi;
+        IResult<InstaCurrentUser> currentUser = null;
+        public string loginResult { get; set; }
+
+        public User(string uname, string pwd) {
+
+            if (uname == "" || pwd == "")
+            {
+                loginResult = "Enter All Fields";
+            }
+            else
+            {
+                var result = Task.Run(() => Login(uname, pwd)).GetAwaiter().GetResult();
+
+                if (result)
+                {
+                    loginResult = "Success";
+                }
+               /* else
+                {
+                    loginResult = "Wrong Password or Email";
+                }*/
+            }
+        }
+
+        private async Task<bool>  Login(string username, string password)
+        {
+            // create user session data and provide login details
+            var userSession = new UserSessionData
+            {
+                UserName = username,
+                Password = password
+            };
+
+            var delay = RequestDelay.FromSeconds(2, 2);
+            // create new InstaApi instance using Builder
+            _instaApi = InstaApiBuilder.CreateBuilder()
+                .SetUser(userSession)
+                .UseLogger(new DebugLogger(LogLevel.Exceptions)) // use logger for requests and debug messages
+                .SetRequestDelay(delay)
+                .Build();
+
+            const string stateFile = "state.bin";
+            try
+            {
+                if (File.Exists(stateFile))
+                {
+                    Console.WriteLine("Loading state from file");
+                    using (var fs = File.OpenRead(stateFile))
+                    {
+                        _instaApi.LoadStateDataFromStream(fs);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            if (!_instaApi.IsUserAuthenticated)
+                {
+                    // login
+                    Console.WriteLine("Logging in as + {userSession.UserName}");
+                    delay.Disable();
+                    var logInResult = await _instaApi.LoginAsync();
+                    delay.Enable();
+                    if (!logInResult.Succeeded)
+                    {
+                        loginResult = "Unable to login:" + logInResult.Info.Message;
+                       return false;
+                    }
+                }
+            return true;
+        }
+
+        public async Task<IResult<InstaUserShortList>> getFollowers()
+        {
+            currentUser = await _instaApi.GetCurrentUserAsync();
+            var followers = await _instaApi.GetUserFollowersAsync(
+                currentUser.Value.UserName, 
+                PaginationParameters.MaxPagesToLoad(5)
+                );
+
+            return followers;
+        }
+
+        public async Task<IResult<InstaUserShortList>> getFollowing() {
+            currentUser = await _instaApi.GetCurrentUserAsync();
+            var following = await _instaApi.GetUserFollowingAsync(
+                currentUser.Value.UserName,
+                PaginationParameters.MaxPagesToLoad(5)
+                );
+            return following;
+        }
+
+        public async Task<IResult<InstaFeed>> getFeed() {
+            var userFeed = await _instaApi.GetUserTimelineFeedAsync(PaginationParameters.MaxPagesToLoad(5));
+            return userFeed;
+        }
+
+        public async Task<IResult<InstaMediaList>> getPosts()
+        {
+            var userPosts = await _instaApi.GetUserMediaAsync(
+                currentUser.Value.UserName, 
+                PaginationParameters.MaxPagesToLoad(5)
+                );
+            return userPosts;
+        }
+
+        public async Task likePost(InstaMedia media) {
+            var likeResult = await _instaApi.LikeMediaAsync(media.InstaIdentifier);
+        }
+
+        public async Task unlikePost(InstaMedia media) {
+            var unlikeResult = await _instaApi.UnLikeMediaAsync(media.InstaIdentifier);
+        }
+    }
+
+}
---
+++ instasharp/ViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instasharp
+{
+    public class ViewModel
+    {
+        private ObservableCollection<Post> posts = new ObservableCollection<Post>();
+        public ObservableCollection<Post> post
+        {
+           get {
+                return posts;
+            }
+        }
+        public ViewModel() {
+            Task.Run(()=>populateFeed());
+            //loadList();
+        }
+
+        public void loadList(){
+         
+            posts.Add(new Post()
+            {
+                likesCount =  300,
+                caption = "This is a caption",
+                commentsCount = "200",
+                userName = "trevortals",
+                url = "/assets/image.jpg",
+                isImage = true
+            });
+
+            posts.Add(new Post()
+            {
+                likesCount = 30,
+                caption = "This is a caption 2",
+                commentsCount = "100",
+                userName = "trevorts",
+                url = @"C:\Users\Trevor Takawira\documents\visual studio 2012\Projects\instasharp\instasharp\assets\image2.jpg",
+                //url = "/assets/image2.jpg",
+                isImage = true
+            });
+
+            posts.Add(new Post()
+            {
+                likesCount = 60,
+                caption = "This is a caption 3",
+                commentsCount = "20",
+                userName = "trs",
+                url = @"C:\Users\Trevor Takawira\documents\visual studio 2012\Projects\instasharp\instasharp\assets\video2.mp4",
+                isImage = false
+            });
+
+            posts.Add(new Post()
+            {
+                likesCount = 30,
+                caption = "This is a caption 2",
+                commentsCount = "100",
+                userName = "trevorts",
+                url = "/assets/video_image.jpg",
+                //url = "/assets/image3.jpg",
+                isImage = true
+            });
+
+            posts.Add(new Post()
+            {
+                likesCount = 30,
+                caption = "This is a caption 2",
+                commentsCount = "100",
+                userName = "trevorts",
+                //url = "/assets/video_image.jpg"
+                url = @"C:\Users\Trevor Takawira\documents\visual studio 2012\Projects\instasharp\instasharp\assets\image4.jpg",
+                isImage = true
+            });
+
+          // MainWindow.icPost.ItemsSource = posts;
+        }
+
+        public async Task populateFeed()
+        {
+            User currentUser = new User("trevortaks", "tsitsiscoco");
+            var feed = await currentUser.getFeed();
+            //List<Post> posts = new List<Post>();
+            if (feed.Succeeded)
+            {
+
+                foreach (var media in feed.Value.Medias)
+                {
+                    int likes = media.LikesCount;
+                    string captionT = media.Caption.Text;
+                    string comments = media.CommentsCount;
+                    string name = media.User.UserName;
+                    List<string> imgURLs = new List<string>();
+                    foreach (var item in media.Images)
+                        imgURLs.Add(item.URI);
+                    posts.Add(new Post()
+                    {
+                        likesCount = likes,
+                        caption = captionT,
+                        commentsCount = comments,
+                        userName = name
+                    });
+                }
+
+            }
+
+        }
+
+    }
+
+    public class Post
+    {
+        public int likesCount { get; set; }
+        public string caption { get; set; }
+        public string commentsCount { get; set; }
+        public string userName { get; set; }
+        public string url { get; set; }
+        public bool isImage { get; set; }
+    }
+}
