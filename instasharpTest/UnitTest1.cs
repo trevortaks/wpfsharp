d6936f47b49e9a6e0d74d3a6b9572372906e9316
Trevor Takawira <trevortaks@gmail.com>
2019-10-15 11:26:52 +0200

    Test Proj

---
+++ instasharpTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using instasharp;
+
+namespace instasharpTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ViewModel vm = new ViewModel();
+            String cb = vm.RandomString();
+            Assert.AreEqual(cb, "ABCDEFGHIJK");
+        }
+    }
+}
